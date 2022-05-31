14210e04c69e3d9aa503efadced1c859819f57ee
AD25 <angelo251158@gmail.com>
2022-05-31 15:56:06 +0200

    if

---
+++ if.py
@@ -0,0 +1,19 @@
+x = 5
+if x < 10:
+    print("5 < 10")
+elif x==10:
+    print("x = 10")
+else:
+    print("nessuna")
+
+#x==10  x!=10   x<10 (> <= >=)
+
+if x>10 and x<20:
+    print("compreso tra 10 e 20")
+
+if x>10 or x<20:
+    print("x minore di 20 o maggiore di 10")
+
+if not(x<10):
+    print("x non è minore di 10")
+
