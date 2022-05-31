e9511d14358c1a957f44487ca421f146bb669bc6
AD25 <angelo251158@gmail.com>
2022-05-31 15:19:56 +0200

    operazioni aritmetiche

---
+++ operazioni_aritmetiche.py
@@ -0,0 +1,12 @@
+x = 5
+y = 9
+#+ - * / % 
+#potenza ** 
+# divisione arrotondata per difetto //
+print(x ** x)
+print(15 // 7)
+
+x = min(5,10) #5
+x = max(5,10) #10
+x = abs(-5) #5, valore assoluto
+x = pow(3,3) #27
