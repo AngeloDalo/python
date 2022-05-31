febf30622d4be395a8e7107a52f27cfd64604ba9
AD25 <angelo251158@gmail.com>
2022-05-31 14:45:03 +0200

    boolean

---
+++ boolean.py
@@ -0,0 +1,27 @@
+from operator import truediv
+
+
+x = True
+y = False
+
+print(5 < 10) #true
+
+if 5<10:
+    print("vero")
+else:
+    print("falso")
+
+print(bool(1)) #true
+print(bool(0)) #false
+
+#False None 0 "" () [] {}
+x = 0
+print(bool(x))
+
+pane = 0 
+if pane:
+    print("c'è pane")
+else:
+    print("non c'è pane")
+
+
