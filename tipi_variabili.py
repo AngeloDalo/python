8c503f314ae07960f901a7946479e5fe6f84fc54
AD25 <angelo251158@gmail.com>
2022-05-31 13:46:10 +0200

    tipi_variabili

---
+++ tipi_variabili.py
@@ -0,0 +1,9 @@
+#list: x = ["roma", "Milano"]
+#tuple: x = ("roma", "Milano")
+#range: x = range(6)
+#dict: x = {"nome": "Luca", "eta": 15}
+#set: x = {"roma", "milano", "napoli"}
+
+x = 5
+print(type(x))
+#stampa <class 'int'>
