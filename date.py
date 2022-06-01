702b35a3aa42e2571baaa74229616bcbb2ad5aa5
AD25 <angelo251158@gmail.com>
2022-06-01 16:32:46 +0200

    date

---
+++ date.py
@@ -0,0 +1,14 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+x = datetime.datetime(2012, 5, 14)
+print(x)
+
+x = datetime.datetime.now()
+print(x.strftime("%B"))
+print(x.strftime("%d/%m/%Y"))
+
+#https://docs.python.org/3/library/datetime.html
+
